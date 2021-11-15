3bff602ea6746e644546670ba522607afaab7800
AdrianDali <hadriandali@gmail.com>
2021-11-15 14:56:37 -0600

    Selection sort terminado con apuntes

--- src/src/SelectionSort.java
+++ src/src/SelectionSort.java
@@ -1,7 +1,11 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * SELECTION SORT 
+The selection sort algorithm sorts an array by repeatedly finding the minimum
+element (considering ascending order) from unsorted part and putting it at the beginning.
+The algorithm  maintains two subarrays in a given array 
+    
+
+
  */
 package src;
 
